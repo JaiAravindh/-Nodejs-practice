cd9e10c0c29492221c0f326f69aef4653d7e2253
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-02 12:57:45 +0530

    Update pipes.js
    This sends HTML page to response

--- pipes.js
+++ pipes.js
@@ -3,15 +3,13 @@ var http = require('http');
 
 http.createServer(function(req,res){
 
-res.writeHead(200, {'Content-Type' : 'text/plain'});
+res.writeHead(200, {'Content-Type' : 'html'});
 
 //creating a readeable stream
-var readSream = fs.createReadStream('file.txt');
+var readSream = fs.createReadStream('index.html');
 
 //using pipe we are sending stream to response 
 readStream.pipe(res);
-
-res.end('Hello world');
 }).listen(3000);
 
 console.log("server is running on http://127.0.0.1:3000")
