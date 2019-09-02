6372d7abc02ae14618fc0daa7ab25cc32a792be6
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-02 08:24:32 +0530

    Update readStream.js
    This code stores the read data into a variable 'data' and once done it prints the content in chunks.

--- Streams/readStream.js
+++ Streams/readStream.js
@@ -8,16 +8,16 @@ var fs = require('fs');
 //lipsum.txt contains 5 paragraphs and 322 characters.
 //'createReadStream' inherits form eventEmitter
 var readStream = fs.createReadStream('lipsum.txt',utf8 );
+var data = '';
 
-readStream.on('data', function(chunk){
-	console.log('-----------------------');
-	console.log(chunk);
-			
-})
-
+readStream.on('data',function(chunk){
+	      //simply printing the data
+	      console.log('-----------------')
+	      data += chunk;
+	      })
 readStream.on('end', function(chunk){
-	console.log('--------end------------');
-})
-
+	      console.log(data)
+	      console.log('--------------end---------');
+})	
 
 
