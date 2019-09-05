6ab5b8f13d10942a0fa17c25dc4003e49d3d103b
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-05 13:25:07 +0530

    Create DBintegration.js

---
+++ expressBasic/DBintegration.js
@@ -0,0 +1,15 @@
+//MONGODB
+
+//npm install mongodb
+
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
+  if (err) throw err
+
+  db.collection('mammals').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
